4368700fc605094ad972a0b5fc6552f413ea4a46
user <evakaboja@icloud.com>
2022-05-20 18:51:49 +0300

    Lab one assignment

---
+++ src/position.java
@@ -0,0 +1,10 @@
+public class position {
+    public static void main (String[] args){
+        double a=-9.81;
+        int t=10;
+        double Vi=0;
+        double Xi=0;
+        double Xt=0.5*a*t*t+Vi*t+Xi;
+        System.out.println("The value of Xt: " + Xt +  "meter");
+    }
+}
